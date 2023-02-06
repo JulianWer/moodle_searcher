d4dacb86a09ea026efcfba5928e2a475de37d04f
pilip-d <philipdell@gmx.de>
2023-02-06 17:42:57 +0100

    Rafactored, fixed db

--- moodle_search/popup/popup.js
+++ moodle_search/popup/popup.js
@@ -1,16 +1,18 @@
 if (typeof browser === "undefined") {
   var browser = chrome;
 }
+
 document.getElementById("reload_button").addEventListener("click", () => {
     console.log("reload button clicked");
     (async () => {
         const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
         await chrome.tabs.sendMessage(tab.id, {name: "reloadMessage"});
     })();
-    });
+});
+
 document.getElementById("search_button").addEventListener("click", () => {
     browser.runtime.sendMessage({name:"searchMessage"});
-    });
+});
 
 
 
--- moodle_search/scripts/contentScript.js
+++ moodle_search/scripts/contentScript.js
@@ -1,32 +1,28 @@
 if (typeof browser === "undefined") {
   var browser = chrome;
 }
-document.addEventListener("DOMContentLoaded", () => {
-  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message);
-      switch(message.name){
-          case "searchMessage":
-              break;
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        switch(message.name){
+            case "searchMessage":
+                break;
             case "reloadMessage":
-              (async () => {
-                dowloadAllPdfs(getArrayOfCorrectPdfUrls());
-              })();
-              
-              break;
+                dowloadAllPdfs(getArrayOfCorrectPdfUrls());            
+                break;
             default:
-              console.error("Message not found");
-      }
-      sendResponse({});
-  })
-});
-
-function getArrayOfCorrectPdfUrls(){
-    let urls = new Array();
-    const a = document.querySelectorAll("a.aalink");
-    for (let i = 0; i < a.length; i++ ) {
-        let regex = new RegExp("moodle.hs-mannheim.de/mod/resource"); 
+                console.error("Message not found");
+        }
+        sendResponse();
+    }
+  );
+  
+  function getArrayOfCorrectPdfUrls(){
+      let urls = new Array();
+      const a = document.querySelectorAll("a.aalink");
+      for (let i = 0; i < a.length; i++ ) {
+          let regex = new RegExp("moodle.hs-mannheim.de/mod/resource"); 
         if(a[i].href.search(regex)>0){
-            urls.push(a[i].href);
+            urls.push({link:a[i].href, key:a[i].innerText});//TODO add Title to key
         }
     }
     return urls;
@@ -42,11 +38,11 @@ function dowloadAllPdfs(urls){
 function downloadPdf(url){
   var xhr = new XMLHttpRequest();
   console.log(url);
-  xhr.open("GET", url);
+  xhr.open("GET", url.link);
   xhr.responseType = "blob";
   xhr.onload = function() {
       if(xhr.status && xhr.status === 200) {
-          savePdf(xhr.response, "download_test");
+          savePdf(xhr.response, "moodle_pdf:"+url.key);
       } 
   }
   xhr.send();
@@ -73,3 +69,16 @@ function savePdf(pdf, key) {
 
 
 
+/*
+PDF IFrame
+const iframeDocument = document.getElementById('pdf-js-viewer').contentWindow;
+let searchText = "TheTextYouWantoToHighlight";
+
+iframeDocument.PDFViewerApplication.pdfViewer.findController.executeCommand('find', {
+    caseSensitive: false, 
+    findPrevious: undefined,
+    highlightAll: true, 
+    phraseSearch: true, 
+    query: searchText
+})
+*/

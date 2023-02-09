14222e8d43066035ea50687e475cbb49cf6ebcf0
julianwernz <2123602@stud.hs-mannheim.de>
2023-02-09 22:33:05 +0100

    working pdf rednering

--- moodle_search/popup/popup.js
+++ moodle_search/popup/popup.js
@@ -2,22 +2,22 @@ import '../pdfjs-3.3.122-dist/build/pdf.js'
 pdfjsLib.GlobalWorkerOptions.workerSrc = '../pdfjs-3.3.122-dist/build/pdf.worker.js';
 
 
-function textFilter(page, query){
+function textFilter(page, query) {
     return page.includes(query);
 }
 
 function getPdfTexts(data, query) {
-    function getTextOfPage(page){
-       return page.getTextContent().then(
-            (textContent) => textContent.items.map((i)=>i.str).join(" ")  
+    function getTextOfPage(page) {
+        return page.getTextContent().then(
+            (textContent) => textContent.items.map((i) => i.str).join(" ")
         )
     }
-    async function getArrayOfPages(pdf){
+    async function getArrayOfPages(pdf) {
         let pages = {};
-        for (let pageNumber=1; pageNumber <= pdf.numPages; pageNumber++){
+        for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
             let page = await pdf.getPage(pageNumber);
             let text = await getTextOfPage(page);
-            if (textFilter(text, query)){
+            if (textFilter(text, query)) {
                 pages[pageNumber] = text;
             }
         }
@@ -30,32 +30,42 @@ function getPdfTexts(data, query) {
     });
 }
 
+function getPage(data){
+    console.log("getPage");
+    pdfjsLib.getDocument(data).promise.then(async (pdf) => 
+        pdf.getPage(1).then(handlePages)
+    )
+}
 
+console.log(await getPdfTexts("sample.pdf", ""));
 // for cross browser support
 let _browser = typeof browser === "undefined" ? chrome : browser;
 console.log("popup is running");
 
 document.getElementById("reload_button").addEventListener("click", () => sendMessage("reloadMessage"));
-document.getElementById("search_button").addEventListener("click", () => sendMessage("searchMessage", {query: getQuery()}));
+document.getElementById("search_button").addEventListener("click", () => sendMessage("searchMessage", { query: getQuery() }));
 document.getElementById("search_button").addEventListener("click", () => sendMessage("getAllSubjects", {}, showResults));
 //document.getElementById("result_cell").addEventListener("click", () => clearDiv("result-container"));
-function showResults(response){
+function showResults(response) {
     document.getElementById("result-container").appendChild(createTable(response));
 }
 
-function createTable(response){
+function createTable(response) {
     let table = document.createElement("table");
     table.setAttribute("id", "results_table");
-    for (let arg of response){
+    for (let arg of response) {
         table.appendChild(createRow(arg));
     }
     return table;
 }
 
-function createRow(arg){
+function createRow(arg) {
     let row = document.createElement("tr");
     let cell = document.createElement("td");
-    cell.onclick = () => clearDiv("result-container");
+    cell.onclick = () => {
+        clearDiv("result-container");
+        getPage("sample.pdf");
+    }
     cell.appendChild(document.createTextNode(arg));
     row.appendChild(cell);
     return row;
@@ -65,13 +75,30 @@ function clearDiv(elementID) {
     document.getElementById(elementID).innerHTML = "";
 }
 
-function sendMessage(name, data = {}, response_handler = null){    
-    _browser.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-        data["name"] = name;
-        _browser.tabs.sendMessage(tabs[0].id, data, response_handler);        
+
+function handlePages(page) {
+    console.log("handlePages");
+    var viewport = page.getViewport({ scale: 1.0 });
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    console.log(canvas);
+    document.getElementById("result-container").appendChild(canvas);
+    let renderContext = { canvasContext: context, viewport: viewport };
+    let renderTask = page.render(renderContext)
+    renderTask.promise.then(function () {
+        console.log('Page rendered');
     });
 }
 
-function getQuery(){
+function sendMessage(name, data = {}, response_handler = null) {
+    _browser.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        data["name"] = name;
+        _browser.tabs.sendMessage(tabs[0].id, data, response_handler);
+    });
+}
+
+function getQuery() {
     return document.getElementById("search_input").value;
 }

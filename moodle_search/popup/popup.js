e02eaf567f7398aa307e69d1c5b42406eb4acec4
pilip-d <philipdell@gmx.de>
2023-02-09 17:02:30 +0100

    changed and fixed pdfjs imports

--- moodle_search/popup/popup.js
+++ moodle_search/popup/popup.js
@@ -1,8 +1,17 @@
-import '../pdf.min.js'
-pdfjsLib.GlobalWorkerOptions.workerSrc = '../pdf.worker.js';
+import '../pdfjs-3.3.122-dist/build/pdf.js'
+pdfjsLib.GlobalWorkerOptions.workerSrc = '../pdfjs-3.3.122-dist/build/pdf.worker.js';
 
-
-console.log(getPDFText("https://dagrs.berkeley.edu/sites/default/files/2020-01/sample.pdf"));
+async function getPdfText(data) {
+    let doc = await pdfjsLib.getDocument(data);
+    let pageTexts = Array.from({length: doc.numPages}, async (_, pageNumber) => {
+        return (
+            await (
+                await doc.getPage(pageNumber+1)
+            ).getTextContent()
+        ).items.map(token => token.str).join('');
+    });
+    return await Promise.all(pageTexts);//filter(pageTexts, (text) => text.includes(query)));
+}
 
 // for cross browser support
 let _browser = typeof browser === "undefined" ? chrome : browser;
@@ -12,6 +21,7 @@ document.getElementById("reload_button").addEventListener("click", () => sendMes
 document.getElementById("search_button").addEventListener("click", () => sendMessage("searchMessage", {query: getQuery()}, showPdf));
 
 function showPdf(response){
+    console.log(getPdfText(response.pdf));
     let iframe = document.createElement("iframe");
     iframe.setAttribute("src", response.pdf+"#page="+response.page);
     iframe.setAttribute("width", "100%");

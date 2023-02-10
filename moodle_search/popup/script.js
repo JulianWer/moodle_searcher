01d65da4e0c68ea3ec8eff064cc7bd4f0015c4db
julianwernz <2123602@stud.hs-mannheim.de>
2023-02-10 11:07:56 +0100

    check if subject exists,if don't store subject

--- moodle_search/popup/script.js
+++ moodle_search/popup/script.js
@@ -21,31 +21,31 @@ _browser.runtime.onMessage.addListener(
 const DBNAME = "MoodleExtensionDB";
 const POSSIBLE_STORES = ["Subjects", "Files"];
 function getDBRequest(reject) {
-      const request = window.indexedDB.open(DBNAME, 1);
-      request.onerror = reject;
-      return request;
+    const request = window.indexedDB.open(DBNAME, 1);
+    request.onerror = reject;
+    return request;
 }
 
 function storeData(storeName, value) {
     return new Promise((resolve, reject) => {
-    const request = getDBRequest(reject);
-    request.onsuccess = (event) => {
-        const db = event.target.result;
-        const transaction = db.transaction(storeName, "readwrite");
-        const objectStore = transaction.objectStore(storeName);
-        console.log(value);
-        const objectStoreRequest = objectStore.put(value);
-        objectStoreRequest.onsuccess = resolve;
-        objectStoreRequest.onerror = reject;
-        objectStoreRequest.onsuccess = (event) => {
-            resolve(event.target.result);
-        }
-    };
-    request.onupgradeneeded = (event) => {
-        const db = event.target.result;
-        for (let store of POSSIBLE_STORES)
-            db.createObjectStore(store, { keyPath: "id", autoIncrement: true });
-    };
+        const request = getDBRequest(reject);
+        request.onsuccess = (event) => {
+            const db = event.target.result;
+            const transaction = db.transaction(storeName, "readwrite");
+            const objectStore = transaction.objectStore(storeName);
+            console.log(value);
+            const objectStoreRequest = objectStore.put(value);
+            objectStoreRequest.onsuccess = resolve;
+            objectStoreRequest.onerror = reject;
+            objectStoreRequest.onsuccess = (event) => {
+                resolve(event.target.result);
+            }
+        };
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            for (let store of POSSIBLE_STORES)
+                db.createObjectStore(store, { keyPath: "id", autoIncrement: true });
+        };
     });
 }
 
@@ -61,6 +61,7 @@ function checkIfSubjectExists(subject) {
                 const result = event.target.result;
                 const subjectExists = result.some((e) => e.name === subject);
                 resolve(subjectExists);
+                console.log(subjectExists);
             }
             objectStoreRequest.onerror = reject;
         };
@@ -83,7 +84,7 @@ function clearDatabase() {
     });
 }
 
-function getAllData (storeName, optionalWhereKeyValuePair = {}) {
+function getAllData(storeName, optionalWhereKeyValuePair = {}) {
     return new Promise((resolve, reject) => {
         const request = getDBRequest(reject);
         request.onsuccess = (event) => {
@@ -110,12 +111,12 @@ function getData(storeName, key) {
     return new Promise((resolve, reject) => {
         const request = getDBRequest(reject);
         request.onsuccess = (event) => {
-        const db = event.target.result;
-        const transaction = db.transaction(POSSIBLE_STORES, "readonly");
-        const objectStore = transaction.objectStore(storeName);
-        const objectStoreRequest = objectStore.get(key);
-        objectStoreRequest.onsuccess = (event) => resolve(event.target.result);
-        objectStoreRequest.onerror = reject;
+            const db = event.target.result;
+            const transaction = db.transaction(POSSIBLE_STORES, "readonly");
+            const objectStore = transaction.objectStore(storeName);
+            const objectStoreRequest = objectStore.get(key);
+            objectStoreRequest.onsuccess = (event) => resolve(event.target.result);
+            objectStoreRequest.onerror = reject;
         };
     });
 }
@@ -139,81 +140,83 @@ function dowloadAllPdfs(urls, subjectID) {
     }
 }
 
-function saveFile(pdf, url, subjectID){
+function saveFile(pdf, url, subjectID) {
     var fileReader = new FileReader();
     fileReader.onload = function (evt) {
-      var result = evt.target.result;
-      storeData(POSSIBLE_STORES[1], {
-        name: url.key,
-        url: url.link,
-        data: result,
-        subjectID: subjectID
-      }).then(() => {
-        console.log("Data stored successfully");
-      }).catch((error) => {
-        console.error("Error storing data:", error);
-      });
+        var result = evt.target.result;
+        storeData(POSSIBLE_STORES[1], {
+            name: url.key,
+            url: url.link,
+            data: result,
+            subjectID: subjectID
+        }).then(() => {
+            console.log("Data stored successfully");
+        }).catch((error) => {
+            console.error("Error storing data:", error);
+        });
     };
     fileReader.readAsDataURL(pdf);
 }
 
-function storeSubject(subject){
-    return storeData(POSSIBLE_STORES[0], {name: subject});
+async function storeSubject(subject) {
+    if (!await checkIfSubjectExists(subject)) {
+        return storeData(POSSIBLE_STORES[0], { name: subject });
+    }
 }
 
-async function getAllPagesFromFile(file){
+async function getAllPagesFromFile(file) {
     let pdf = await pdfjsLib.getDocument(file.data).promise;
     let pages = [];
-    for (let pageNumber=1; pageNumber <= pdf.numPages; pageNumber++){
+    for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
         pages.push(await pdf.getPage(pageNumber));
     }
     return pages;
 }
 
-async function getAllFilesOfSubject(subject){
-    return  (await getAllData(POSSIBLE_STORES[1], {subjectID: subject.id}));
+async function getAllFilesOfSubject(subject) {
+    return (await getAllData(POSSIBLE_STORES[1], { subjectID: subject.id }));
 }
 
-async function getAllSubjects(){
+async function getAllSubjects() {
     return await getAllData(POSSIBLE_STORES[0]);
 }
 
-async function getTextOfPage(page){
-    return (await page.getTextContent()).items.map((i)=>i.str).join(" ");
+async function getTextOfPage(page) {
+    return (await page.getTextContent()).items.map((i) => i.str).join(" ");
 }
 
-async function pageMatchesQuery(page, query){
+async function pageMatchesQuery(page, query) {
     return false;
     // TODO create query language options (OR, AND, NOT, ..., Case Sesnsitive etc)
     return (await getTextOfPage(page)).toLowerCase().replace(" ", "").includes(query.toLowerCase().replace(" ", ""));
 }
 
-async function fileMatchesQuery(file, query){
+async function fileMatchesQuery(file, query) {
     return containsFilter(await getAllPagesFromFile(file), async (page) => await pageMatchesQuery(page, query));
 }
 
-async function subjectMatchesQuery(subject, query){
+async function subjectMatchesQuery(subject, query) {
     return containsFilter(await getAllFilesOfSubject(subject), async (file) => await fileMatchesQuery(file, query));
 }
 
-async function containsFilter(iter, filter){
+async function containsFilter(iter, filter) {
     // returns true if filter is true for at least one element of iter
-    for (let i of iter){
-        if (await filter(await i)){
+    for (let i of iter) {
+        if (await filter(await i)) {
             return true;
         }
     }
     return false;
 }
 
-export async function getAllPagesFromFileOfQuery(file, query){
+export async function getAllPagesFromFileOfQuery(file, query) {
     return (await getAllPagesFromFile(file)).filter(async (page) => await pageMatchesQuery(page, query));
 }
 
-export async function getAllFilesFromSubjectOfQuery(subject, query){
-    return (await getAllFilesOfSubject(subject)).filter(async (file)=> await fileMatchesQuery(file, query));
+export async function getAllFilesFromSubjectOfQuery(subject, query) {
+    return (await getAllFilesOfSubject(subject)).filter(async (file) => await fileMatchesQuery(file, query));
 }
 
-export async function getAllSubjectsOfQuery(query){
-    return (await getAllSubjects()).filter(async (subject)=> await subjectMatchesQuery(subject, query));
+export async function getAllSubjectsOfQuery(query) {
+    return (await getAllSubjects()).filter(async (subject) => await subjectMatchesQuery(subject, query));
 }

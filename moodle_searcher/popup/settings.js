fc28b9f1d891f7155da2725220289ebbcfb8177a
pilip-d <philipdell@gmx.de>
2023-02-17 15:38:33 +0100

    added js for moodleUrl setting

---
+++ moodle_searcher/popup/settings.js
@@ -0,0 +1,12 @@
+import {setMoodleUrl, getMoodleUrl} from "./moodleUrl.js";
+
+const INPUT = document.getElementById("moodle-url-button");
+const SAVE_BUTTON = document.getElementById("change-moodle-url-button");
+SAVE_BUTTON.addEventListener("click", (_)=>setMoodleUrl(INPUT.value));
+INPUT.addEventListener("keydown", (e)=>{
+    if (e.key === "Enter"){
+        setMoodleUrl(INPUT.value);
+    }
+});
+
+INPUT.value = await getMoodleUrl();

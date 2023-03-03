da17f1f2a254fc5f21bfa81d5ed247cca1aba572
Julian Wernz <julian24.12@gmx.de>
2023-03-03 23:43:59 +0100

    formated

--- moodle_searcher/popup/scripts/popup.js
+++ moodle_searcher/popup/scripts/popup.js
@@ -1,222 +1,229 @@
 // TODO mark found words in pdfs
-// TODO show status of download (blink/rotate while not finished etc)
 // TODO maybe add little arrow in each subject and file row
 // TODO show container with animation etc
 
 import {
-    getAllSubjectsOfQuery,
-    getAllFilesFromSubjectOfQuery,
-    getAllPagesFromFileOfQuery,
-    clearDatabase
-} from './script.js';
-import '../../pdfjs-3.3.122-dist/build/pdf.js';
+  getAllSubjectsOfQuery,
+  getAllFilesFromSubjectOfQuery,
+  getAllPagesFromFileOfQuery,
+  clearDatabase,
+} from "./script.js";
+import "../../pdfjs-3.3.122-dist/build/pdf.js";
 import { initMoodleUrl, getMoodleTabId } from "./moodleUrl.js";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = '../pdfjs-3.3.122-dist/build/pdf.worker.js';
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  "../pdfjs-3.3.122-dist/build/pdf.worker.js";
 
 // for cross browser support
 let _browser = typeof browser === "undefined" ? chrome : browser;
 console.log("popup is running");
 
-
 const CACHE = {
-    subjects: [],
-    files: [],
-    query: ""
-}
+  subjects: [],
+  files: [],
+  query: "",
+};
 
 let currentActiveContainer = -1; // -1 = empty, 0 = subjects, 1 = files, 2 = pages
 let CONTENT_CONTAINERS = [
-    document.getElementById("subject-container"),
-    document.getElementById("files-container"),
-    document.getElementById("pages-container")
+  document.getElementById("subject-container"),
+  document.getElementById("files-container"),
+  document.getElementById("pages-container"),
 ];
 
 hideButtons();
 hideAllContainers();
 await initMoodleUrl();
 
-
 document.getElementById("reload-button").addEventListener("click", () => {
-    rotateReloadImage();
-    sendMessage("reloadMessage");      
+  rotateReloadImage();
+  sendMessage("reloadMessage");
 });
 document.getElementById("search-button").addEventListener("click", async () => {
+  await updateQuery();
+  await showSubjectsFromQuery();
+});
+document.body.addEventListener("keypress", async (e) => {
+  if (e.key === "Enter") {
     await updateQuery();
     await showSubjectsFromQuery();
+  }
 });
-document.body.addEventListener('keypress', async (e) => {
-    if (e.key === 'Enter') {
-        await updateQuery();
-        await showSubjectsFromQuery();
-    }
-});
-document.getElementById("prev-button").addEventListener("click", () => showPreviousTable());
+document
+  .getElementById("prev-button")
+  .addEventListener("click", () => showPreviousTable());
 document.getElementById("clear-button").addEventListener("click", async () => {
-    await clearDatabase();
-    await showSubjectsFromQuery();
+  await clearDatabase();
+  await showSubjectsFromQuery();
 });
 
-_browser.runtime.onMessage.addListener(
-    async(message, _, sendResponse) => {
-      switch (message.name) {
-        case "downloadDone":
-            stopRotattionReloadImage();
-          break;
-        default:
-          console.error("Message not found");
-      }
-    }
-  );
+_browser.runtime.onMessage.addListener(async (message, _, sendResponse) => {
+  switch (message.name) {
+    case "downloadDone":
+      stopRotattionReloadImage();
+      break;
+    default:
+      console.error("Message not found");
+  }
+});
 
 function rotateReloadImage() {
-    let reloadButton = document.getElementById("reload-image");
-    reloadButton.className = "rotated";
+  // added class rotated is defined in style.css
+  let reloadButton = document.getElementById("reload-image");
+  reloadButton.className = "rotated";
 }
 function stopRotattionReloadImage() {
-    let reloadButton = document.getElementById("reload-image");
-    reloadButton.className = "reload-image";
+  let reloadButton = document.getElementById("reload-image");
+  reloadButton.className = "reload-image";
 }
 
 function hideAllContainers() {
-    for (let container of CONTENT_CONTAINERS) {
-        container.style.display = "none";
-    }
+  for (let container of CONTENT_CONTAINERS) {
+    container.style.display = "none";
+  }
 }
 
 function showContainer(_) {
-    hideAllContainers();
-    CONTENT_CONTAINERS[currentActiveContainer].style.display = "block";
+  hideAllContainers();
+  CONTENT_CONTAINERS[currentActiveContainer].style.display = "block";
 }
 
 function updateQuery() {
-    CACHE.query = document.getElementById("search-input").value;
+  CACHE.query = document.getElementById("search-input").value;
 }
 
 function clearDiv(elementID) {
-    document.getElementById(elementID).innerHTML = "";
+  document.getElementById(elementID).innerHTML = "";
 }
 
 function createRow(name, onclick) {
-    let row = document.createElement("li");
-    row.onclick = onclick;
-    row.appendChild(document.createTextNode(name));
-    return row;
+  let row = document.createElement("li");
+  row.onclick = onclick;
+  row.appendChild(document.createTextNode(name));
+  return row;
 }
 
 function showList(valueAndEventList, container_id) {
-    clearDiv(container_id);
-    let table = document.createElement("ul");
-    //table.classList.add("material-element");
-    table.classList.add("content-list");
-    table.classList.add("no-scrollbar");
-    for (let [value, event] of valueAndEventList)
-        table.appendChild(createRow(value, event));
-    let container = document.getElementById(container_id);
-    container.appendChild(table);
-    hideAllContainers();
-    container.style.display = "block";
+  clearDiv(container_id);
+  let table = document.createElement("ul");
+  //table.classList.add("material-element");
+  table.classList.add("content-list");
+  table.classList.add("no-scrollbar");
+  for (let [value, event] of valueAndEventList)
+    table.appendChild(createRow(value, event));
+  let container = document.getElementById(container_id);
+  container.appendChild(table);
+  hideAllContainers();
+  container.style.display = "block";
 }
 
 function showPreviousTable() {
-    switch (currentActiveContainer) {
-        case 1:
-            currentActiveContainer = 0;
-            showContainer("subjects-container");
-            break;
-        case 2:
-            currentActiveContainer = 1;
-            showContainer("files-container");
-            break;
-        default:
-            hideButtons();
-            hideAllContainers();
-            break;
-    }
+  switch (currentActiveContainer) {
+    case 1:
+      currentActiveContainer = 0;
+      showContainer("subjects-container");
+      break;
+    case 2:
+      currentActiveContainer = 1;
+      showContainer("files-container");
+      break;
+    default:
+      hideButtons();
+      hideAllContainers();
+      break;
+  }
 }
 
-
-function showButtons() { // TODO rename
-    let button = document.getElementById("prev-button");
-    button.disabled = false;
-    button.classList.remove("disabled");
-    button.style.cursor = "pointer";
+function showButtons() {
+  // TODO rename
+  let button = document.getElementById("prev-button");
+  button.disabled = false;
+  button.classList.remove("disabled");
+  button.style.cursor = "pointer";
 }
 
-function hideButtons() { // TODO rename
-    let button = document.getElementById("prev-button");
-    button.disabled = true;
-    button.classList.add("disabled");
-    button.style.cursor = "default";
+function hideButtons() {
+  // TODO rename
+  let button = document.getElementById("prev-button");
+  button.disabled = true;
+  button.classList.add("disabled");
+  button.style.cursor = "default";
 }
 
 async function showSubjectsFromQuery() {
-    let subjects = await getAllSubjectsOfQuery(CACHE.query);
-    CACHE.subjects = subjects;
-    showSubjects(subjects);
+  let subjects = await getAllSubjectsOfQuery(CACHE.query);
+  CACHE.subjects = subjects;
+  showSubjects(subjects);
 }
 
 function showSubjects(subjects) {
-    showButtons();
-    currentActiveContainer = 0;
-    showList(subjects.map((subject) => [
-        subject.name,
-        () => showFilesOfSubjectFromQuery(subject)
-    ]), "subject-container");
+  showButtons();
+  currentActiveContainer = 0;
+  showList(
+    subjects.map((subject) => [
+      subject.name,
+      () => showFilesOfSubjectFromQuery(subject),
+    ]),
+    "subject-container"
+  );
 }
 
 async function showFilesOfSubjectFromQuery(subject) {
-    let files = await getAllFilesFromSubjectOfQuery(subject, CACHE.query);
-    CACHE.files = files;
-    currentActiveContainer = 1;
-    showList(files.map((file) => [
-        file.name,
-        () => {
-            currentActiveContainer = 2;
-            clearDiv("pages-container")
-            showFile(file);
-            showContainer("pages-container");
-        }
-    ]), "files-container");
+  let files = await getAllFilesFromSubjectOfQuery(subject, CACHE.query);
+  CACHE.files = files;
+  currentActiveContainer = 1;
+  showList(
+    files.map((file) => [
+      file.name,
+      () => {
+        currentActiveContainer = 2;
+        clearDiv("pages-container");
+        showFile(file);
+        showContainer("pages-container");
+      },
+    ]),
+    "files-container"
+  );
 }
 
 function showFile(file) {
-    getAllPagesFromFileOfQuery(file, CACHE.query).then((pages) => {
-        if (pages.length > 0) {
-            renderPages(pages, file)
-        }
-    });
+  getAllPagesFromFileOfQuery(file, CACHE.query).then((pages) => {
+    if (pages.length > 0) {
+      renderPages(pages, file);
+    }
+  });
 }
 
 function renderPages(pages, file) {
-    pdfjsLib.getDocument(file.url).promise.then((pdf) => {
-        for (let page of pages) {
-            renderPage(pdf, 1 + parseInt(page.pageNumber), file.url);
-        }
-    });
+  pdfjsLib.getDocument(file.url).promise.then((pdf) => {
+    for (let page of pages) {
+      renderPage(pdf, 1 + parseInt(page.pageNumber), file.url);
+    }
+  });
 }
 
 function renderPage(pdf, pageNumber, fileUrl) {
-    pdf.getPage(pageNumber).then(function (page) {
-        let viewport = page.getViewport({ scale: 1.0 });
-        let canvas = getCanvas(viewport.width, viewport.height, () => window.open(fileUrl + "#page=" + pageNumber));
-        document.getElementById("pages-container").appendChild(canvas);
-        page.render({
-            canvasContext: canvas.getContext('2d'),
-            viewport: viewport
-        });
-        //highlightQueryOnRenderedPage(page);
+  pdf.getPage(pageNumber).then(function (page) {
+    let viewport = page.getViewport({ scale: 1.0 });
+    let canvas = getCanvas(viewport.width, viewport.height, () =>
+      window.open(fileUrl + "#page=" + pageNumber)
+    );
+    document.getElementById("pages-container").appendChild(canvas);
+    page.render({
+      canvasContext: canvas.getContext("2d"),
+      viewport: viewport,
     });
+    //highlightQueryOnRenderedPage(page);
+  });
 }
 
 function getCanvas(width, height, onclickHandler) {
-    let canvas = document.createElement("canvas");
-    canvas.height = height;
-    canvas.width = width;
-    canvas.onclick = onclickHandler
-    return canvas;
+  let canvas = document.createElement("canvas");
+  canvas.height = height;
+  canvas.width = width;
+  canvas.onclick = onclickHandler;
+  return canvas;
 }
-
 
 // async function highlightQueryOnRenderedPage(page) {
 //     let query = CACHE.query;
@@ -267,9 +274,7 @@ function getCanvas(width, height, onclickHandler) {
 //     });
 // }
 
-
-
 async function sendMessage(name, data = {}, response_handler = null) {
-    data["name"] = name;
-    _browser.tabs.sendMessage(await getMoodleTabId(), data, response_handler);
+  data["name"] = name;
+  _browser.tabs.sendMessage(await getMoodleTabId(), data, response_handler);
 }
--- moodle_searcher/scripts/contentScript.js
+++ moodle_searcher/scripts/contentScript.js
@@ -1,45 +1,45 @@
 // for cross browser support
 let _browser = typeof browser === "undefined" ? chrome : browser;
 
-
-_browser.runtime.onMessage.addListener(
-  async(message, _, sendResponse) => {
-    switch (message.name) {
-      case "reloadMessage":
-        sendMessage("downloadSubject", { subject: getHeading(), files: await getArrayOfCorrectPdfUrls() }, function(response) {
-          console.log(response);
-        });
-        sendMessage("downloadDone");
-        break;
-      default:
-        console.error("Message not found");
-    }
+_browser.runtime.onMessage.addListener(async (message, _, sendResponse) => {
+  switch (message.name) {
+    case "reloadMessage":
+      sendMessage("downloadSubject", {
+        subject: getHeading(),
+        files: await getArrayOfCorrectPdfUrls(),
+      });
+      sendMessage("downloadDone");
+      break;
+    default:
+      console.error("Message not found");
   }
-);
+});
 
 function getHeading() {
   const heading = document.querySelector(".page-header-headings");
-  return (heading && heading.childElementCount > 0)
+  return heading && heading.childElementCount > 0
     ? heading.firstChild.innerText
     : "no heading";
 }
 
 function getArrayOfCorrectPdfUrls() {
-  const links = Array.from(document.querySelectorAll("a.aalink.stretched-link"));
- 
+  const links = Array.from(
+    document.querySelectorAll("a.aalink.stretched-link")
+  );
+
   function isLinkToPDF(a) {
     return new Promise(function (resolve, reject) {
       var xhr = new XMLHttpRequest();
-      xhr.open('HEAD', a.href, true);
+      xhr.open("HEAD", a.href, true);
 
       xhr.onload = function () {
-        var contentType = xhr.getResponseHeader('Content-Type');
-        var bool = contentType === 'application/pdf';
+        var contentType = xhr.getResponseHeader("Content-Type");
+        var bool = contentType === "application/pdf";
         resolve(bool);
       };
 
       xhr.onerror = function () {
-        reject(new Error('Network error'));
+        reject(new Error("Network error"));
       };
 
       xhr.send();
@@ -48,14 +48,16 @@ function getArrayOfCorrectPdfUrls() {
   function formatLink(a) {
     return {
       url: a.href,
-      key: a.innerText
-    }
+      key: a.innerText,
+    };
   }
-  return Promise.all(links.map(function (link) {
-    return isLinkToPDF(link).then(function (bool) {
-      return bool ? formatLink(link) : null;
-    });
-  })).then(function (filteredLinks) {
+  return Promise.all(
+    links.map(function (link) {
+      return isLinkToPDF(link).then(function (bool) {
+        return bool ? formatLink(link) : null;
+      });
+    })
+  ).then(function (filteredLinks) {
     return filteredLinks.filter(function (link) {
       return link !== null;
     });
